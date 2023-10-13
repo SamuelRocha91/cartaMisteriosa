e64653f6339686d8d2e14397c6af859630334166
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-10-13 11:06:46 -0300

    adiciona logica de adicionar texto em caso de o input nao ser preenchido corretamente

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-function addCarta() {
-  const button = document.querySelector('button');
+function criarCarta(event) {
   const input = document.querySelector('input');
   const p = document.querySelector('p');
-  button.addEventListener('click', (event) => {
-    event.preventDefault();
-    p.innerHTML = '';
-    const words = input.value.split(' ');
-    for (let i = 0; i < words.length; i += 1) {
-      const span = document.createElement('span');
-      span.id = 'criar-carta';
-      span.innerText = `${words[i]}`;
-      p.appendChild(span);
-    }
-  });
+  event.preventDefault();
+  p.innerHTML = '';
+  if (!input.value || input.value.trim().length === 0) {
+    const span = document.createElement('span');
+    span.innerText = 'Por favor, digite o conteúdo da carta.';
+    return p.appendChild(span);
+  }
+  const words = input.value.split(' ');
+  for (let i = 0; i < words.length; i += 1) {
+    const span = document.createElement('span');
+    span.id = 'criar-carta';
+    span.innerText = `${words[i]}`;
+    p.appendChild(span);
+    p.innerHTML += ' ';
+  }
+}
+
+function addCarta() {
+  const button = document.querySelector('button');
+  button.addEventListener('click', (event) => criarCarta(event));
 }
 
 window.onload = () => {
